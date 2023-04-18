09ad278ac286ed0410f2fb4f56c0692c78ac2717
DESKTOP-KM2AE57\user <dudtjq>
2023-04-18 18:46:13 +0900

    [이영섭] 1. petit Desert 프로젝트 관련 파일 이미지 api 수정 2. petit Desert 프로젝트 관련 jsp , controller 수정 petitDesertProject #6

--- src/main/java/com/petitdesert/page/config/WebMVCConfig.java
+++ src/main/java/com/petitdesert/page/config/WebMVCConfig.java
@@ -1,10 +1,12 @@
 package com.petitdesert.page.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.petitdesert.page.common.FileManagerService;
 
+@Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
 	
 	@Override
